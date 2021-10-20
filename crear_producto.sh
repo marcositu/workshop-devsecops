8f2711d6e044c489b26f90874f54c28e2b1b7438
Marcos <marcositu@gmail.com>
2021-10-20 10:33:06 -0300

    Update crear_producto.sh

--- crear_producto.sh
+++ crear_producto.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Permite crear un producto dentro de DefectDojo
+
 curl -s -X 'POST' -H 'Authorization: Token 9010a9fe648b30b59bdb72a156edaf0285c90a8e' \
   'http://localhost:8082/api/v2/products/' \
   -H 'accept: application/json' \
