7d002063e9b3418d6a5b891d5d39213cbbae719b
Marcos <marcositu@gmail.com>
2021-10-20 10:32:38 -0300

    Update crear_engagement.sh

--- crear_engagement.sh
+++ crear_engagement.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Permite crear un engagement dentro de un producto en DefectDojo
+
 PROYECTO=`curl -X 'GET' 'http://localhost:8082/api/v2/products/' -H 'accept: application/json' -H 'Authorization: Token 9010a9fe648b30b59bdb72a156edaf0285c90a8e' -s | jq '.results[] | select(.name=="demos") | .id'`
 
 curl -X 'POST' -H 'Authorization: Token 9010a9fe648b30b59bdb72a156edaf0285c90a8e' \
